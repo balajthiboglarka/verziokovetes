9a948fefdad0a2552e716db99092dc46955b8e05
Hektor Salvador <salvador.vh05@gmail.com>
2026-04-23 11:48:49 +0000

    feat: adatréteg hozzáadása - ProductViewModel és HotcakesProductService
    - ProductViewModel POCO: Id, Name, Price, ImageUrl, CategoryIds
    - HotcakesProductService: SQL lekérdezés hcc_Product, hcc_ProductTranslations,
      hcc_ProductXCategory táblákból, kép URL összeállítása bvin alapján
    - csproj frissítve: új Compile bejegyzések a két új fájlhoz

---
+++ Ceramista.Dnn.HelloWorld/Models/ProductViewModel.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace Ceramista.Dnn.Ceramista.Dnn.HelloWorld.Models
+{
+    public class ProductViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public List<string> CategoryIds { get; set; }
+
+        public ProductViewModel()
+        {
+            CategoryIds = new List<string>();
+        }
+    }
+}
---
+++ Ceramista.Dnn.HelloWorld/Services/HotcakesProductService.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using Ceramista.Dnn.Ceramista.Dnn.HelloWorld.Models;
+
+namespace Ceramista.Dnn.Ceramista.Dnn.HelloWorld.Services
+{
+    public class HotcakesProductService
+    {
+        private static string ConnectionString
+        {
+            get { return WebConfigurationManager.ConnectionStrings["SiteSqlServer"].ConnectionString; }
+        }
+
+        public List<ProductViewModel> GetAllProducts()
+        {
+            var products = new Dictionary<string, ProductViewModel>(StringComparer.OrdinalIgnoreCase);
+
+            const string productSql = @"
+                SELECT DISTINCT
+                    p.bvin,
+                    pt.ProductName,
+                    p.SitePrice,
+                    p.ImageFileSmall
+                FROM hcc_Product p
+                JOIN hcc_ProductTranslations pt ON pt.ProductId = p.bvin
+                WHERE p.Status = 1
+                  AND p.StoreId = 1
+                  AND pt.Culture = 'hu-HU'
+                ORDER BY pt.ProductName";
+
+            using (var conn = new SqlConnection(ConnectionString))
+            using (var cmd = new SqlCommand(productSql, conn))
+            {
+                conn.Open();
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var id = reader["bvin"].ToString();
+                        var imageFile = reader["ImageFileSmall"] as string;
+                        products[id] = new ProductViewModel
+                        {
+                            Id = id,
+                            Name = reader["ProductName"].ToString(),
+                            Price = Convert.ToDecimal(reader["SitePrice"]),
+                            ImageUrl = !string.IsNullOrWhiteSpace(imageFile)
+                                ? string.Format("/Portals/0/Hotcakes/Data/products/{0}/small/{1}", id, imageFile)
+                                : null
+                        };
+                    }
+                }
+            }
+
+            if (products.Count == 0)
+                return new List<ProductViewModel>();
+
+            const string catSql = @"
+                SELECT pxc.ProductId, pxc.CategoryId
+                FROM hcc_ProductXCategory pxc
+                INNER JOIN hcc_Product p ON p.bvin = pxc.ProductId
+                WHERE p.Status = 1 AND p.StoreId = 1";
+
+            using (var conn = new SqlConnection(ConnectionString))
+            using (var cmd = new SqlCommand(catSql, conn))
+            {
+                conn.Open();
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var productId = reader["ProductId"].ToString();
+                        ProductViewModel product;
+                        if (products.TryGetValue(productId, out product))
+                        {
+                            product.CategoryIds.Add(reader["CategoryId"].ToString());
+                        }
+                    }
+                }
+            }
+
+            return new List<ProductViewModel>(products.Values);
+        }
+    }
+}
